1939a9ea4b7151278f4c08a1bec071d1fa46b716
bbarnefiher <bb222@pvsd.org>
2019-03-04 11:49:34 -0500

    This is what we have as a baseline, and what Brian/Alex worked on that one day Friday.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,34 @@
 package com.company;
+import sun.plugin.javascript.navig.Array;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws FileNotFoundException {
+        //Trying to read from textfile
+        File teamInfo = new File("teamInfo.txt");
+        Scanner teamScanner = new Scanner(teamInfo);
+        teamScanner.useDelimiter(",");
+	    ArrayList<Team> teams = new ArrayList<Team>();
+        String teamName = teamScanner.next();
+	    String temp = teamScanner.next();
+	    double winLoss = Double.parseDouble(temp);
+
+	    teams.add(new Team(teamName,winLoss));
+        System.out.println(teams.get(0).name);
+
+
     }
 }
+//ArrayList<String> text = new ArrayList<>();
+//        while (teamScanner.hasNextLine()){
+//            text.add(teamScanner.nextLine());
+//            lineCount++;
+//
+//
+//        }
+//        for (int i = 0; i < lineCount; i++) {
+//            System.out.println(text.get(i));
+//        }
---
+++ src/com/company/Team.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Team
+    {
+        public String name;
+        public double winLoss;
+
+        public Team(String name, double winLoss)
+            {
+                this.name = name;
+                this.winLoss = winLoss;
+            }
+    }
