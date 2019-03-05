a3f2c762e53c3d0e42c5d2fdea1f0e18d2afc557
eg913 <eg913@pvsd.org>
2019-03-05 11:12:09 -0500

    scanner works

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,15 +8,23 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
         //Trying to read from textfile
-        File teamInfo = new File("teamInfo.txt");
-        Scanner teamScanner = new Scanner(teamInfo);
+       
+        Scanner teamScanner = new Scanner(new File("teamInfo.txt"));
         teamScanner.useDelimiter(",");
 	    ArrayList<Team> teams = new ArrayList<Team>();
         String teamName = teamScanner.next();
-	    String temp = teamScanner.next();
-	    double winLoss = Double.parseDouble(temp);
+        String temp = teamScanner.next();
+        double winLoss = Double.parseDouble(temp);
+        teams.add(new Team(teamName, winLoss));
+	    while (teamScanner.hasNext()) {
+             teamName = teamScanner.next();
+             temp = teamScanner.next();
+            //System.out.println("t" + temp);
+            //System.out.println("x" + teamName);
+             winLoss = Double.parseDouble(temp);
 
-	    teams.add(new Team(teamName,winLoss));
+            teams.add(new Team(teamName.substring(1), winLoss));
+        }
         System.out.println(teams.get(0).name);
 
 
