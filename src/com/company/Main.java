67a9a6d5cf2ed0aac1818f44a7e1a522a6b8022d
eg913 <eg913@pvsd.org>
2019-03-13 11:22:29 -0400

    not done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,17 +15,23 @@ public class Main {
         String teamName = teamScanner.next();
         String temp = teamScanner.next();
         double winLoss = Double.parseDouble(temp);
-        teams.add(new Team(teamName, winLoss));
+        String temp2 = teamScanner.next();
+        double SRS = Double.parseDouble(temp2);
+        teams.add(new Team(teamName, winLoss, SRS));
         while (teamScanner.hasNext()) {
             teamName = teamScanner.next();
             temp = teamScanner.next();
+            temp2 = teamScanner.next();
             //System.out.println("t" + temp);
             //System.out.println("x" + teamName);
             winLoss = Double.parseDouble(temp);
+            SRS = Double.parseDouble(temp2);
 
-            teams.add(new Team(teamName.substring(2), winLoss));
+
+            teams.add(new Team(teamName.substring(2), winLoss, SRS));
         }
-       // System.out.println(teams.get(0).name);
+
+        // System.out.println(teams.get(0).name);
 
         ArrayList<String> TeamsList = new ArrayList<String>();
         Scanner teamsListScanner = new Scanner(new File("competingTeams.txt"));
@@ -38,11 +44,13 @@ public class Main {
 //
        // ArrayList<String> competingString = new ArrayList<>();
         ArrayList<Team> competingTeams = new ArrayList<>();
-
+        System.out.println(teams.get(0).name);
         for (int i = 0; i < TeamsList.size(); i++)
         {
             for(int j = 0; j < teams.size(); j++)
             {
+                System.out.println(TeamsList.get(i));
+                System.out.println(teams.get(j).name);
                 if (TeamsList.get(i).equalsIgnoreCase(teams.get(j).name))
                 {
                     competingTeams.add(teams.get(j));
@@ -59,7 +67,8 @@ public class Main {
             competingTeams = tempTeams;
             System.out.println(tempTeams.size());
         }
-    }//g
+    //g
+    }
 }
 //ArrayList<String> text = new ArrayList<>();
 //        while (teamScanner.hasNextLine()){
--- src/com/company/Team.java
+++ src/com/company/Team.java
@@ -4,13 +4,16 @@ public class Team
 {
     public String name;
     public double winLoss;
+    public double SRS;
 
-    public Team(String name, double winLoss)
+    public Team(String name, double winLoss, double teamSRS)
     {
         this.name = name;
         this.winLoss = winLoss;
+        this.SRS = (teamSRS + 30) / 60;
     }
     public String getName(){
         return name;
     }
+
 }
